71b6469b999a2c792c00b15ffee14eed0a132742
Lucas <lucas@Ashleys-MacBook-Pro.local>
2019-06-05 16:46:30 -0600

    Updated js submission

--- practice.js
+++ practice.js
@@ -115,20 +115,22 @@ user2.email = 'bryan.smith@devmounta.in'
   Create an empty object called methodCollection.
 */
 
-//Code Here
+let methodCollection = {}
 
 /*
   Now add two methods (functions that are properties on objects) to your methodCollection object.
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
 */
 
-//Code Here
+methodCollection.alertHello = function alertHello () {alert ('hello')}
+methodCollection.logHello = function logHell0 () {console.log('hello')}
 
 /*
   Now call your alertHello and logHello methods.
 */
 
-//Code Here
+methodCollection.alertHello
+methodCollection.logHello
 
 ////////// PROBLEM 6 //////////
 
@@ -137,7 +139,10 @@ user2.email = 'bryan.smith@devmounta.in'
   Return a new object with all of the information that you passed in.
 */
 
-//Code Here
+function makePerson (name, birthday, ssn) {
+  let newObj = {name, birthday, ssn}
+  return newObj
+}
 
 ////////// PROBLEM 7 //////////
 
@@ -146,4 +151,7 @@ user2.email = 'bryan.smith@devmounta.in'
   Return that object so that whenever you invoke makeCard, you get a brand new credit card.
 */
 
-//Code Here
+function makeCard (cardNumber, expirationDate, securityCode) {
+  let newObj = {cardNumber, expirationDate, securityCode}
+  return newObj
+}
